1ba720af531d27b012c5dd964ed8acf26c233ac5
huangziang <huangziang_2024@qq.com>
2026-07-10 00:28:26 +0800

    refactor(radar_bridge): node callback use LidarLocation msg, fill all fields
    - Subscribe to radar_interfaces::msg::LidarLocation (was PoseWithCovarianceStamped)
    - Publish radar_interfaces::msg::GameState (was std_msgs::String)
    - sub_lidar_pose_callback: fill all 24 fields (was only opponent_hero)
      opponent/ally: hero, engineer, infantry_3/4, aerial, sentry x/y
    - pub_game_state_callback: fill all 5 fields + fix duplicate stage_remain_time
    - Add lidar_location_ and game_state_ as class members
    - Add destructor with override to stop ZMQ threads

--- ros_ws/src/radar_bridge/include/radar_bridge/radar_bridge_node.hpp
+++ ros_ws/src/radar_bridge/include/radar_bridge/radar_bridge_node.hpp
@@ -1,6 +1,8 @@
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "radar_bridge/zmq_bridge.hpp"
 #include "radar_bridge/zmq_data_format.hpp"
+#include "radar_interfaces/msg/game_state.hpp"
+#include "radar_interfaces/msg/lidar_location.hpp"
 #include "std_msgs/msg/string.hpp"
 #include <atomic>
 #include <expected>
@@ -14,15 +16,18 @@ namespace radar_bridge::node {
 class RadarBridgeNode final : public rclcpp::Node {
 public:
     RadarBridgeNode();
-    auto sub_lidar_pose_callback(geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg,
-        radar_bridge::zmqdata::pub::LidarLocation& lidar_location)
+    ~RadarBridgeNode() override;
+    auto sub_lidar_pose_callback(const radar_interfaces::msg::LidarLocation& msg)
         -> std::expected<void, std::string>;
-    auto pub_game_state_callback(radar_bridge::zmqdata::sub::TransmitGameState& game_state)
-        -> std::expected<void, std::string>;
+    auto pub_game_state_callback() -> std::expected<void, std::string>;
 
 private:
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_game_state_;
-    rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr sub_lidar_pose_;
+    rclcpp::Publisher<radar_interfaces::msg::GameState>::SharedPtr game_state_publisher_;
+    rclcpp::Subscription<radar_interfaces::msg::LidarLocation>::SharedPtr lidar_pose_subscription_;
+
+    radar_bridge::zmqdata::pub::LidarLocation lidar_location_{};
+    radar_bridge::zmqdata::sub::TransmitGameState game_state_{};
+
     std::atomic<bool> zmqpub_thread_running_ { false };
     std::atomic<bool> zmqsub_thread_running_ { false };
 };
