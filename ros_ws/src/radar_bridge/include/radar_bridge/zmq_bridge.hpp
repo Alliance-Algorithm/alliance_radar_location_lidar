23f128000b77b4e8f79c8bdaff0c17ce83ce5710
huangziang <huangziang_2024@qq.com>
2026-07-12 20:26:59 +0800

    refactor(zmq_bridge): internalize atomic flags, simplify thread signatures

--- ros_ws/src/radar_bridge/include/radar_bridge/zmq_bridge.hpp
+++ ros_ws/src/radar_bridge/include/radar_bridge/zmq_bridge.hpp
@@ -21,21 +21,19 @@ public:
         -> std::expected<void, std::string>;
     auto zmqsub(radar_bridge::zmqdata::sub::TransmitGameState& game_state_)
         -> std::expected<void, std::string>;
-    auto zmqsub_thread(std::atomic<bool>& zmqsub_thread_running_,
-        radar_bridge::zmqdata::sub::TransmitGameState& game_state_)
+    auto zmqpub_thread(const radar_bridge::zmqdata::pub::LidarLocation& lidarlocation_data)
         -> std::expected<void, std::string>;
-    auto zmqpub_thread(std::atomic<bool>& zmqpub_thread_running_,
-        const radar_bridge::zmqdata::pub::LidarLocation& lidarlocation_data)
+    auto zmqsub_thread(radar_bridge::zmqdata::sub::TransmitGameState& game_state_)
         -> std::expected<void, std::string>;
-    auto zmqpub_thread_stop(std::atomic<bool>& zmqpub_thread_running_)
-        -> std::expected<void, std::string>;
-    auto zmqsub_thread_stop(std::atomic<bool>& zmqsub_thread_running_)
-        -> std::expected<void, std::string>;
+    auto zmqpub_thread_stop() -> std::expected<void, std::string>;
+    auto zmqsub_thread_stop() -> std::expected<void, std::string>;
 
 private:
     std::mutex zmq_mutex_;
     std::thread zmqpub_thread_;
     std::thread zmqsub_thread_;
+    std::atomic<bool> zmqpub_thread_running_{false};
+    std::atomic<bool> zmqsub_thread_running_{false};
 
     zmq::context_t context_;
     zmq::socket_t publisher_;
