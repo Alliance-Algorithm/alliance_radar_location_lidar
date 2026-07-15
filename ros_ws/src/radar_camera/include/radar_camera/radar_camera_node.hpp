2707ad9aea5895726617589320e20c8bfcec49fd
huangziang <huangziang_2024@qq.com>
2026-07-15 10:57:57 +0800

    feat(radar_camera): complete communication framework
    - ConfigsLoader: load enemy_color, class_id, model_path, intrinsics, extrinsics, topic_name
    - ImageCallback: capture cv::Mat image and steady_clock timestamp from hikcamera
    - PublishCallback: build CameraDetectionPose msg with 6 robot slots + confidence
    - Remove Eigen dependency (use std::vector<double> for config fields)
    - Add radar_interfaces dependency to package.xml

--- ros_ws/src/radar_camera/include/radar_camera/radar_camera_node.hpp
+++ ros_ws/src/radar_camera/include/radar_camera/radar_camera_node.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "radar_interfaces/msg/camera_detection_pose.hpp"
-#include <Eigen/Dense>
 #include <chrono>
 #include <expected>
 #include <map>
@@ -9,6 +8,7 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <string>
 #include <utility>
+#include <vector>
 namespace radar_camera {
 
 typedef struct CameraConfig {
@@ -19,13 +19,26 @@ typedef struct CameraConfig {
     int infantry_4_class_id;
     int sentry_class_id;
     std::string model_path;
-    Eigen::Matrix3d camera_matrix;
-    Eigen::Vector<double, 5> distortion_coefficients;
-    Eigen::Vector3d rotation;
-    Eigen::Vector3d translation;
+    std::vector<double> camera_matrix;
+    std::vector<double> distortion_coefficients;
+    std::vector<double> rotation;
+    std::vector<double> translation;
     std::string topic_name;
 } camera_inference_config;
-
+typedef struct RobotPose {
+    cv::Point hero_position;
+    double hero_confidence;
+    cv::Point engine_position;
+    double engine_confidence;
+    cv::Point infantry_3_position;
+    double infantry_3_confidence;
+    cv::Point infantry_4_position;
+    double infantry_4_confidence;
+    cv::Point sentry_position;
+    double sentry_confidence;
+    cv::Point drone_position;
+    double drone_confidence;
+} robot_pose;
 class RadarCameraNode final : public rclcpp::Node {
 public:
     RadarCameraNode();
@@ -33,13 +46,11 @@ public:
     auto ConfigsLoader(rclcpp::Node& node, camera_inference_config& config)
         -> std::expected<void, std::string>;
     auto ImageCallback(sensor_msgs::msg::Image::SharedPtr msg) -> void;
-    auto PublishCallback(const std::vector<Eigen::Vector2d>& robot_poses) -> void;
-
-private:
+    auto PublishCallback(const robot_pose& robot_poses) -> void;
     cv::Mat ImageData;
     std::chrono::steady_clock::time_point capture_timestamp_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_subscription_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pose_publisher_;
+    rclcpp::Publisher<radar_interfaces::msg::CameraDetectionPose>::SharedPtr pose_publisher_;
     camera_inference_config config_;
 };
 
