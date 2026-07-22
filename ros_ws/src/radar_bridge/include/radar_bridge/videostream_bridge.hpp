f92fb88be288f2d7bc166e78601b0caa46a0b208
Yukikaze2233 <yingziyuw@gmail.com>
2026-07-23 01:56:25 +0800

    refactor(bridge): consume camera frames with persistent SHM reader
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- ros_ws/src/radar_bridge/include/radar_bridge/videostream_bridge.hpp
+++ ros_ws/src/radar_bridge/include/radar_bridge/videostream_bridge.hpp
@@ -1,13 +1,12 @@
 #pragma once
 
+#include <hikcamera/shared_frame_reader.hpp>
+
 #include <atomic>
-#include <chrono>
 #include <expected>
 #include <string>
 #include <thread>
 #include <zmq.hpp>
-
-#include <opencv2/core.hpp>
 
 namespace radar_bridge::videostream_bridge {
 
@@ -16,22 +15,21 @@ public:
     VideoBridge() = default;
     ~VideoBridge();
 
-    auto video_init(const std::string& shm_name, const std::string& pub_address, int width,
-        int height) -> std::expected<void, std::string>;
+    auto video_init(const std::string& shm_name,
+        const std::string& pub_address) -> std::expected<void, std::string>;
     auto video_thread() -> std::expected<void, std::string>;
     auto video_thread_stop() -> std::expected<void, std::string>;
 
 private:
-    int shm_fd_ = -1;
-    std::string shm_name_;
-    std::string pub_address_;
-    int width_  = 0;
-    int height_ = 0;
+    hikcamera::SharedFrameReader reader_;
 
-    zmq::context_t ctx_ { 1 };
-    zmq::socket_t pub_ { ctx_, zmq::socket_type::pub };
+    std::string pub_address_;
+    std::string shm_name_;
+
+    zmq::context_t ctx_{1};
+    zmq::socket_t pub_{ctx_, zmq::socket_type::pub};
     std::thread video_thread_;
-    std::atomic<bool> video_thread_running_ { false };
+    std::atomic<bool> video_thread_running_{false};
 };
 
 } // namespace radar_bridge::videostream_bridge
